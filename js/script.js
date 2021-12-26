1a1ab0021654cbd8341b64799f52b10cadb0d91b
Mit Shah <mitshah2406@gmail.com>
2021-12-26 20:30:05 +0530

    Made Skills ,projects ,services & contact section and fixed home page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let links = document.querySelectorAll('.navlist a');
-console.log(links);
+// console.log(links);
 
 for (let i = 0; i < links.length; i++) {
     links[i].addEventListener('mouseover',()=>{
@@ -8,12 +8,6 @@ for (let i = 0; i < links.length; i++) {
     
 }
 
-let searchbtn = document.querySelector('#searchbtn');
-let search = document.querySelector('#search');
-searchbtn.addEventListener('click',()=>{
-search.classList.toggle('active')
-search.setAttribute('placeholder', 'Search...')
-})
 
 let navlist = document.querySelector('.navlist');
 let hamburgerButton = document.querySelector('#hamburger');
@@ -23,6 +17,66 @@ hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('fa-times');
 })
 
+let navbar = document.querySelector('.navbar');
+window.addEventListener('scroll',()=>{
+    if(window.scrollY>10 && window.scrollY<740){
+        navbar.classList.add('white');
+    }
+    else{
+        navbar.classList.remove('white');
+    }
+    if(window.scrollY>740){
+        navbar.classList.add('normal');    
+    }
+    else{
+        navbar.classList.remove('normal');
+    }
+    
+})
 
+// ----- skills --------------------------------
+
+let skillContent = document.querySelectorAll('.skill_content');
+let skillHeader = document.querySelectorAll('.skill_header');
+console.log(skillContent);
+function toggleSkills () {
+    let itemClass = this.parentNode.className
+    for (let i = 0; i < skillContent.length; i++) {
+        skillContent[i].className = 'skill_content skill_close'
+    }
+    if(itemClass === 'skill_content skill_close'){
+        this.parentNode.className = 'skill_content skill_open'
+    }
+}
+skillHeader.forEach((element)=>{
+element.addEventListener('click', toggleSkills)
+})
+
+var swiper = new Swiper(".projects-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    grabCusror: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
 
 
--- js/textAnime.js
+++ js/textAnime.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Student", "Programmer", "Coder", "Web Developer"];
+const textArray = ["Student", "Programmer", "Coder", "Developer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
